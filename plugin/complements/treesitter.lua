ac2873c4d8283c4e806894df6cb5cea453924ddb
Miguel Angel <miguel9196.guzman@gmail.com>
2023-06-23 18:46:14 -0500

    retoques_nvim_2

--- plugin/complements/treesitter.lua
+++ plugin/complements/treesitter.lua
@@ -42,7 +42,11 @@ require'nvim-treesitter.configs'.setup {
     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
+      colors = {"#E53681",
+                "#0FFFEE",
+                "#0c9e00",
+                "#ffffff",
+        }, -- table of hex strings
     -- termcolors = {} -- table of colour name strings
   },
 
