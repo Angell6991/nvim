41ab03d998c1c89facb9ea24900568954e75bfa7
Angell6991 <miguel9196.guzman@gmail.com>
2024-07-22 20:45:47 -0500

    regrsition

--- templates-Python/regresion_lineal_sql.py
+++ templates-Python/regresion_lineal_sql.py
@@ -1,22 +1,38 @@
 import os
 import numpy as np
 import pandas as pd
+import sqlalchemy as sql
 import matplotlib.pyplot as plt
 import sklearn.metrics as sk_mec
 import sklearn.linear_model as sk_lineal
 
 os.system("clear")
 
+######################################################
+##----------------load_data_and_table---------------##
+######################################################
+
+#-------------inf_para_la_conexion_con_qtl-----------#
+user    =   "root"
+host    =   "localhost"
+password    =   "password"
+database    =   "datos_01"
+
+#--------------------creando_conexion----------------#
+conex   =   sql.create_engine(
+        f"mysql+mysqlconnector://{user}:{password}@{host}/{database}"
+        )
+
 #------------------Imporat_table---------------------#
-tabla   =  pd.read_csv("ruta/datos.dat", delimiter="  ", header = None, engine = 'python') 
+tabla   =   pd.read_sql_table("lineal", con=conex)
 
 ######################################################
 ##--------------manipulacion_de_datos---------------##
 ######################################################
 
 #-------------separando_datos_de_la_tabla------------#
-x_datos =   np.array(tabla[0]).reshape(-1,1)
-y_datos =   np.array(tabla[1]).reshape(-1,1)
+x_datos =   tabla.drop(columns=["var_y"])
+y_datos =   tabla["var_y"]
 
 #------------------regresion_lineal------------------#
 regre   = sk_lineal.LinearRegression()
@@ -38,8 +54,8 @@ print("confianza coeficiente: ", sk_mec.r2_score(y_datos, y_regre))
 ######################################################
 
 #-----------------datos_á_graficar-------------------#
-x_graf = np.array(tabla[0])
-y_graf = np.array(tabla[1])
+x_graf = x_datos
+y_graf = y_datos
 
 x_graf_reg  =   x_datos
 y_graf_reg  =   y_regre
@@ -47,8 +63,8 @@ y_graf_reg  =   y_regre
 #---------creando_y_modificando_la_grafica-----------#
 plt.figure(facecolor="#1D1D1D")
 
-plt.plot(x_graf_reg, y_graf_reg, color="#00CCDE", label= r"$f(x) = $" + str(m) + r"$x + $" + str(b), zorder=1)    
-plt.scatter(x_graf, y_graf, color="#E53681", marker="o",zorder =2)
+plt.plot(x_graf_reg, y_graf_reg, color="#00CCDE", label= r"$f(x) = $" + str(m) + r"$x + $" + str(b))    
+plt.scatter(x_graf, y_graf, color="#E53681", marker="o")
 plt.grid(True, linestyle="dashed", color="#ffffff", alpha=0.5)
 
 plt.xlabel("x", color="#5AEDA3")
