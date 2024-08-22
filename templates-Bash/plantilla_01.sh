6edddac8c8c1859331a79c81d4497ddfdeda7582
Angell6991 <miguel9196.guzman@gmail.com>
2024-08-22 13:01:22 -0500

    bash_02

--- templates-Bash/plantilla_01.sh
+++ templates-Bash/plantilla_01.sh
@@ -3,22 +3,33 @@
 ####################################
 ##---------define_variable--------##
 ####################################
+
 variable=2
 echo    $variable
+
+##-----variable_in_terminal-------##
+
+read    var_01
+echo    $var_01
 
 ####################################
 ##--------define_ciclo_for--------##
 ####################################
+
 for i in {1..8}
-do 
-    echo    hola mundo $i
-done
+    do 
+        echo    hola mundo $i
+    done
+
 ##------ciclo_for_one_line--------##
+
 for i in {1..8}; do echo    hola mundo $i ;done
 
 ##-------ciclo_for_C-style--------##
 
 for ((i=1; i<=5; i=i+2)) 
-do
-    echo "$i"
-done
+    do
+        echo "$i"
+    done
+
+
