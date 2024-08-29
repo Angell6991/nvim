2582cde4e2909babf5db8dbdb28597429803c318
Angell6991 <miguel9196.guzman@gmail.com>
2024-08-28 23:57:59 -0500

    lualine_plugin

--- plugin/complements/lualine.lua
+++ plugin/complements/lualine.lua
@@ -1,50 +1,69 @@
 require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = "moonfly", -- Puedes mantener o cambiar esto según tu preferencia
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
+    options = {
+        icons_enabled = true,
+        -- theme = "codedark", 
+        -- theme = "wombat", 
+        -- theme = "papercolor_dark", 
+        theme = "horizon", 
+        -- theme = "moonfly", 
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        }
     },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {
-      'filename',
-      {
-        'buffers', 
-        icons_enabled = true, 
-        max_length = vim.o.columns / 4, -- Ajusta el tamaño según sea necesario
-        symbols = { alternate_file = '󰣙 ', modified = '󰆓 ', pinned = ' ' },
-        filetype_names = { ['lua'] = 'Lua', ['python'] = 'Python' }
-      }
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {},
+        -- lualine_c = {'filename',},
+        -- lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_x = {
+            {
+                'buffers', 
+                icons_enabled = true, 
+                max_length = vim.o.columns / 2, -- Ajusta el tamaño según sea necesario
+                symbols = { 
+                    alternate_file = '󰅏 ', 
+                    modified = ' 󰆓 ', 
+                    pinned = '  ', 
+                },
+                -- filetype_names = { 
+                --     ['lua'] = 'Lua', 
+                --     ['python'] = 'Py',
+                --     ['sh'] = 'Sh',
+                --     ['dat'] = 'dat',
+                --     ['c'] = 'C',
+                --     ['cpp'] = 'C++',
+                --     ['tex'] = 'Tex',
+                -- }
+            },
+            'fileformat', 
+            'filetype',
+        },
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
     },
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {}
 }
 
